84daed8a51396a31bfd5862214fe4b12a4891667
IsaacSavona <isaacsavona@gmail.com>
2024-10-30 19:38:38 +0100

    commiting some stuff

--- votingsystemsim/State.py
+++ votingsystemsim/State.py
@@ -1,3 +1,7 @@
+import numpy as np
+
 class State:
-    def __init__(self):
-        self
+    def __init__(self, population: int, electoral_votes: int, popular_votes: NDArray[int]):
+        self.population = population
+        self.electoral_votes = electoral_votes
+        self.popular_votes = popular_votes
